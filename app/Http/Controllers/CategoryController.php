9b377991c8a60ace417d30e65e70b139397628c6
nguik-bareng <ucupu7553@gmail.com>
2022-11-01 15:59:48 +0700

    membuat validasi category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -26,6 +26,11 @@ class CategoryController extends Controller
 
     public function store(Request $request) {
        
+
+        $validated = $request->validate([
+            'name' => 'required|unique:categories|max:255',
+        ]);
+
         $category = Category::create($request->all());
         return redirect('category');
     }
--- resources/views/category-add.blade.php
+++ resources/views/category-add.blade.php
@@ -9,6 +9,15 @@
             <p>Category Add</p>
               <!-- /.card -->
               <div class="card">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <div class="card-body">
                     <form action="category-add" method="post">
                         @csrf
